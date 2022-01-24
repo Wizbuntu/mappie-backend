22269d324fc17934a97668ad10d660ad3e1eba39
Chukwuemeka Wisdom <guruwiz2@gmail.com>
2022-01-24 11:57:04 +0100

    Modificiation

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -14,10 +14,14 @@ const {
 // require express validator
 const { check } = require('express-validator')
 
+// require verifyAuth
+const verifyAuth = require('../middleware/verifyAuth')
+
 
 
 // create category route ---- [POST]
 router.post('/create/category',
+    verifyAuth,
     check('title').notEmpty().withMessage("Title is required").bail().isString().withMessage("Title type mismatch"),
     check('slug').notEmpty().withMessage("Slug is required").bail().isSlug().withMessage("Invalid slug"),
     check('formSchema').notEmpty().withMessage("Form schema is required").bail().isArray().withMessage("Form schema type mismatch"),
@@ -26,15 +30,16 @@ router.post('/create/category',
 
 
 // fetch all categories --- [GET]
-router.get('/all/categories', fetchCategories)
+router.get('/all/categories', verifyAuth, fetchCategories)
 
 
 // fetch single category ---- [GET]
-router.get('/category/:categoryId', fetchSingleCategory)
+router.get('/category/:categoryId', verifyAuth, fetchSingleCategory)
 
 
 // update single category --- [PUT]
 router.put('/update/category/:categoryId',
+    verifyAuth,
     check('title').notEmpty().withMessage("Title is required").bail().isString().withMessage("Title type mismatch"),
     check('slug').notEmpty().withMessage("Slug is required").bail().isSlug().withMessage("Invalid slug"),
     check('formSchema').notEmpty().withMessage("Form schema is required").bail().isArray().withMessage("Form schema type mismatch"),
--- routes/metadata.routes.js
+++ routes/metadata.routes.js
@@ -14,8 +14,13 @@ const {
 const { check } = require('express-validator')
 
 
+// require verifyAuth
+const verifyAuth = require('../middleware/verifyAuth')
+
+
 // create metaData route ----- [POST]
 router.post('/create/metadata',
+    verifyAuth,
     check("categoryId").notEmpty().withMessage("Category ID is required").bail().isMongoId().withMessage("Category id type mismatch"),
     check("country").notEmpty().withMessage("Country is required").bail().isString().withMessage("Country type mismatch"),
     check("latitude").notEmpty().withMessage("Latitude is required").bail().isNumeric().withMessage("Latitude type mismatch"),
@@ -25,20 +30,20 @@ router.post('/create/metadata',
 
 
 // Fetch metaData by category Id
-router.get('/metadata/category/:categoryId', metadataByCategory)
+router.get('/metadata/category/:categoryId', verifyAuth, metadataByCategory)
 
 
 // fetch all metadata ---- [GET]
-router.get('/all/metadata', fetchAllMetaData)
+router.get('/all/metadata', verifyAuth, fetchAllMetaData)
 
 
 
 // search metaData
-router.get('/search/metadata', searchMetaData)
+router.get('/search/metadata', verifyAuth, searchMetaData)
 
 
 // delete metaData
-router.delete('/delete/metadata/:metadataId', deleteMetaData)
+router.delete('/delete/metadata/:metadataId', verifyAuth, deleteMetaData)
 
 
 
